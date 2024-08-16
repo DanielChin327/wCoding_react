2a94221d12ba4baf4718bcf72ad47bcfdd1d950e
Daniel Chin <DanChin327@gmail.com>
2024-08-16 13:34:14 +0900

    cont practice React

--- exercise/counter/src/assets/components/Counter/Counter.jsx
+++ exercise/counter/src/assets/components/Counter/Counter.jsx
@@ -11,12 +11,26 @@ const Counter = () => {
     setCount (count - 1)
   }
 
+  const countDouble = () => {
+    setCount(count * 2)
+  }
+
+  const countHalf = () => {
+    setCount(count / 2)
+  }
+
+  const reset = () => {
+    setCount(0)
+  }
   return (
 
     <div>
       <h2>Counter: {count} </h2>
         <button onClick = {countUp}>+</button>
         <button onClick = {countDown}>-</button>
+        <button onClick = {countDouble}>x2</button>
+        <button onClick = {countHalf}>/2</button>
+        <button onClick = {reset}>Reset</button>
     </div>
 
   )
@@ -25,3 +39,6 @@ const Counter = () => {
 
 
 }
+
+
+export default Counter;
